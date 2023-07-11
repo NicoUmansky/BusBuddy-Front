40aaa3d8829eefebcc223d829037ac13f4c11c68
NicoUmansky <umanskynico@gmail.com>
2023-07-11 18:59:00 -0300

    predict input text

--- BusBuddy-FrontEnd/llamadaColectivo/pantallaPrincipal.js
+++ BusBuddy-FrontEnd/llamadaColectivo/pantallaPrincipal.js
@@ -1,9 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from "./alertaChofer.module.css";
 
 
 const PantallaPrincipal = () => {
   const [address, setAddress] = useState("");
+
+  useEffect(() => {
+    initAutoComplete();
+  }, []);
+
+  const initAutoComplete = () => {
+    if (google && google.maps) {
+      const input = document.getElementById("inputUbi");
+      const options = {
+        componentRestrictions: { country: "ar" }
+};
+      const autocomplete = new google.maps.places.Autocomplete(input, options);
+
+      autocomplete.addListener('place_changed', () => {
+        const place = autocomplete.getPlace();
+        if (place && place.formatted_address) {
+          setAddress(place.formatted_address);
+        }
+      });
+    }
+  };
 
   const getLocation = (e) => {
     e.preventDefault();
@@ -12,23 +33,23 @@ const PantallaPrincipal = () => {
         // console.log(position.coords.latitude, position.coords.longitude);
         const latitude = position.coords.latitude;
         const longitude = position.coords.longitude;
-  
+
         fetch("http://localhost:3001/findLocation/" + latitude + "," + longitude)
           .then(response => response.json())
           .then(response => {
             const address = String(response).split(",")[0];
             setAddress(address);
-            document.getElementById("inputUbi").setAttribute("defaultValue", address);
+            document.getElementById("inputUbi").setAttribute("value", address);
             const map = document.querySelector("iframe").setAttribute("src", "https://maps.google.com/maps?q=" + address + "&t=&z=13&ie=UTF8&iwloc=&output=embed");
           });
-        
+
       });
-    } 
+    }
     else {
       alert("Geolocation is not supported by this browser.");
     }
   };
-  
+
   return (
     <div>
       <iframe
@@ -36,13 +57,13 @@ const PantallaPrincipal = () => {
         src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d64335.07666590948!2d-58.44772896163354!3d-34.580385620490944!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sGalp%C3%B3n%20de%20Ropa!5e0!3m2!1ses!2sar!4v1661524962941!5m2!1ses!2sar"
       ></iframe>
       <form className={styles.container}>
-        <input
+      <input
           type="text"
           placeholder="Añadir ubicación"
-          onChange={(e) => setAddress(e.target.value)}
-          value={address}
-          id='inputUbi'
+          defaultValue={address}
+          id="inputUbi"
           className={styles.inputUbi}
+          autocomplete="off"
         />
         <button id="btnUbiActual" className={styles.btnUbiActual} onClick={getLocation}><b>Ubicación Actual</b></button>
         <label className={styles.text}> ó </label>
--- BusBuddy-FrontEnd/pages/_document.tsx
+++ BusBuddy-FrontEnd/pages/_document.tsx
@@ -1,9 +1,17 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import { config } from 'dotenv'
+config()
+console.log(process.env.NEXT_PUBLIC_API_KEY)
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <script
+            src={`https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_API_KEY}&libraries=places`}
+            async
+            defer
+         />    
       <body>
         <Main />
         <NextScript />
