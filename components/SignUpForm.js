651bc093d747d7b465c11d4e2d08f2357f742ef3
Nicolas Umansky <umanskynico@gmail.com>
2023-08-14 16:33:39 -0300

    cambio de rutas back hosteado

--- components/SignUpForm.js
+++ components/SignUpForm.js
@@ -28,7 +28,7 @@ const SignUpForm = () => {
     e.preventDefault();
     
     if (password == RepeatPassword){
-      const newUser = fetch("https://breakable-turtleneck-shirt-foal.cyclic.app/CreateUser", {
+      const newUser = fetch("http://breakable-turtleneck-shirt-foal.cyclic.app/CreateUser", {
         method: "POST",
         body: JSON.stringify({
           nombreapellido: nombreapellido,
