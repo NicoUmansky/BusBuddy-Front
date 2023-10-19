7097468fe76d6c43798bb9cbd32de42319c5262f
Octavio Spano <101335584+OctavioSpano@users.noreply.github.com>
2023-10-19 14:28:59 -0300

    creando el manifest.json

--- next.config.js
+++ next.config.js
@@ -1,3 +1,14 @@
+const withPWA = require('next-pwa');
+
+module.exports = withPWA({
+  pwa: {
+    dest: 'public',
+    register: true,
+    skipWaiting: true,
+  },
+  
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
