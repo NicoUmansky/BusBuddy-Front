8746dfeff4d2aadb2d9434abdd278da9bca428fd
Nicolas Umansky <umanskynico@gmail.com>
2023-10-23 15:16:22 -0300

    intento de pwa

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,49 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { UserProvider } from "../components/UserContext";
+import type { Metadata } from "next";
 
+const APP_NAME = "BusBuddy";
+const APP_DEFAULT_TITLE = "BusBuddy: Viajar sin limites";
+const APP_TITLE_TEMPLATE = "%s - PWA App";
+const APP_DESCRIPTION = "BusBuddy: Viajar sin limites";
+
+export const metadata: Metadata = {
+  applicationName: APP_NAME,
+  title: {
+    default: APP_DEFAULT_TITLE,
+    template: APP_TITLE_TEMPLATE,
+  },
+  description: APP_DESCRIPTION,
+  manifest: "/manifest.json",
+  themeColor: "#FFFFFF",
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "default",
+    title: APP_DEFAULT_TITLE,
+    // startUpImage: [],
+  },
+  formatDetection: {
+    telephone: false,
+  },
+  openGraph: {
+    type: "website",
+    siteName: APP_NAME,
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
+  twitter: {
+    card: "summary",
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
+};
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <UserProvider>
