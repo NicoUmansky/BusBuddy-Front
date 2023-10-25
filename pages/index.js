00bb9df6a648d586df038a699104d9b4c11daefe
Octavio Spano <101335584+OctavioSpano@users.noreply.github.com>
2023-10-25 14:33:19 -0300

    por poner el menu en la pag antes del llamar colectivo

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useRouter } from 'next/router';
 import SignUpForm from '../components/SignUpForm';
-import "../components/SignUpForm.module.css"
+import "../components/SignUpForm.module.css";
 
 const HomePage = () => {
   const router = useRouter();
