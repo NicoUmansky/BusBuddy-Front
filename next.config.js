2fe4df951b221987b36984856047b18a24fa25ee
NicoUmansky <umanskynico@gmail.com>
2023-10-25 22:52:23 -0300

    production env

--- next.config.js
+++ next.config.js
@@ -8,6 +8,7 @@ module.exports = nextConfig
 
 const withPWA = require("next-pwa")({
   dest: "public",
+  disable: process.env.NODE_ENV === "development",
   register: true,
   skipWaiting: true,
 });
