bd66ccfc05be537f9bb08c57b1762bcb3701c221
NicoUmansky <umanskynico@gmail.com>
2023-10-26 19:47:44 -0300

    quiero que bb sea seguroo

--- next.config.js
+++ next.config.js
@@ -1,22 +1,9 @@
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
-
-module.exports = nextConfig
-
-const withPWA = require("next-pwa")({
-  dest: "public",
-  disable: process.env.NODE_ENV === "development",
-  register: true,
-  skipWaiting: true,
-});
+const withPWA = require('next-pwa')({
+	dest: 'public',
+	register: true,
+	skipWaiting: true,
+})
 
 module.exports = withPWA({
-  reactStrictMode: true,
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
-  },
-});
+	reactStrictMode: true,
+})
